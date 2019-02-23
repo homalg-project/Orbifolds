70b465871531be396816b96e5cec0d6f3ed93534
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2019-02-23 10:52:31 +0100

    start to comply with new AutoDoc

--- gap/GIT-fan.gd
+++ gap/GIT-fan.gd
@@ -16,61 +16,70 @@
 #
 ####################################
 
-# basic operations:
+#! @Chapter GIT-fans
 
-DeclareOperationWithDocumentation( "Is_aFace",
-        [ IsList, IsHomalgModule ],
-        [ "check if the torus orbit corresponding to <A>gamma_0</A> meets the vanishing set of the ideal <A>a</A>" ],
-        "<C>true</C> or <C>false</C>",
-        "gamma_0, a",
-        [ "GIT-fans", "a-faces" ] );
+#! @Section a-faces
 
-DeclareAttributeWithDocumentation( "aFaces",
-        IsHomalgModule,
-        [ "Compute the torus orbits intersecting the vanishing set of <A>a</A>" ],
-        "a list of lists of indices",
-        "a",
-        [ "GIT-fans", "a-faces" ] );
+#! @Description
+#!  Check if the torus orbit corresponding to <A>gamma_0</A> meets the vanishing set of the ideal <A>a</A>.
+#! @Arguments gamma_0, a
+#! @Returns <C>true</C> or <C>false</C>
+DeclareOperation( "Is_aFace",
+        [ IsList, IsHomalgModule ] );
 
-DeclareOperationWithDocumentation( "aFaces",
-        [ IsHomalgModule, IsInt ],
-        [ "Compute the torus orbits of dimension <A>i</A> intersecting the vanishing set of <A>a</A>" ],
-        "a list of lists of indices",
-        "a,i",
-        [ "GIT-fans", "a-faces" ] );
 
-DeclareAttributeWithDocumentation( "OrbitCones",
-        IsHomalgGradedModule,
-        [ "Compute the full dimensional orbit cones of the homogeneous ideal <A>a</A>" ],
-        "a list of cones",
-        "a",
-        [ "GIT-fans", "Orbit_cones" ] );
+#! @Description
+#!  Compute the torus orbits intersecting the vanishing set of <A>a</A>.
+#! @Arguments a
+#! @Returns a list of lists of indices
+DeclareAttribute( "aFaces",
+        IsHomalgModule );
 
-DeclareOperationWithDocumentation( "OrbitCones",
-        [ IsHomalgGradedModule, IsInt ],
-        [ "Compute the orbit cones of the homogeneous ideal <A>a</A>" ],
-        "a list of cones",
-        "a,i",
-        [ "GIT-fans", "Orbit_cones" ] );
+#! @Description
+#!  Compute the torus orbits of dimension <A>i</A> intersecting the vanishing set of <A>a</A>.
+#! @Arguments a,i
+#! @Returns a list of lists of indices
+DeclareOperation( "aFaces",
+        [ IsHomalgModule, IsInt ] );
 
-DeclareOperationWithDocumentation( "GIT_Cone",
-        [ IsHomalgGradedModule, IsList ],
-        [ "Compute the GIT cone of the given vector <A>w</A> w.r.t. the homogeneous ideal <A>a</A>" ],
-        "a cone",
-        "a, w",
-        [ "GIT-fans", "GIT-cone" ] );
+#! @Section Orbit cones
 
-DeclareOperationWithDocumentation( "GIT_Cone",
-        [ IsHomalgGradedModule, IsHomalgModuleElement ],
-        [ "Compute the GIT cone of the given vector <A>w</A> w.r.t. the homogeneous ideal <A>a</A>" ],
-        "a cone",
-        "a, w",
-        [ "GIT-fans", "GIT-cone" ] );
+#! @Description
+#!  Compute the full dimensional orbit cones of the homogeneous ideal <A>a</A>.
+#! @Arguments a
+#! @Returns a list of cones
+DeclareAttribute( "OrbitCones",
+        IsHomalgGradedModule );
 
-DeclareAttributeWithDocumentation( "GIT_Fan",
-        IsHomalgGradedModule,
-        [ "Compute the GIT fan of the vanishing set of the homogeneous ideal <A>a</A>",
-          "under the action of the of the torus corresponding to the grading of the the underlying ring" ],
-        "a fan",
-        "a",
-        [ "GIT-fans", "GIT-fan" ] );
+#! @Description
+#!  Compute the orbit cones of the homogeneous ideal <A>a</A>.
+#! @Arguments a,i
+#! @Returns a list of cones
+DeclareOperation( "OrbitCones",
+        [ IsHomalgGradedModule, IsInt ] );
+
+#! @Section GIT-cone
+
+#! @Description
+#!  Compute the GIT cone of the given vector <A>w</A> w.r.t. the homogeneous ideal <A>a</A>.
+#! @Arguments a, w
+#! @Returns a cone
+DeclareOperation( "GIT_Cone",
+        [ IsHomalgGradedModule, IsList ] );
+
+#! @Description
+#!  Compute the GIT cone of the given vector <A>w</A> w.r.t. the homogeneous ideal <A>a</A>.
+#! @Arguments a, w
+#! @Returns a cone
+DeclareOperation( "GIT_Cone",
+        [ IsHomalgGradedModule, IsHomalgModuleElement ] );
+
+#! @Section GIT-fan
+
+#! @Description
+#!  Compute the GIT fan of the vanishing set of the homogeneous ideal <A>a</A>
+#!  under the action of the of the torus corresponding to the grading of the the underlying ring.
+#! @Arguments a
+#! @Returns a fan
+DeclareAttribute( "GIT_Fan",
+        IsHomalgGradedModule );
